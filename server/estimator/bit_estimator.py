4e5685ca9d147f18d5cb1e23dcefb31f0e1b8182
Milad Golgolnia <golgolniamilad@gmail.com>
2023-07-11 04:57:18 +0330

    1. Add my questions. 2. Change Estimator to support replicated data.

--- server/estimator/bit_estimator.py
+++ server/estimator/bit_estimator.py
@@ -14,12 +14,18 @@ class Server:
         self.sum_of_users_of1 = 0
         self.sum_v_ofh = 0
         self.sum_of_users_ofh = 0
+        self.replica_sum_v_of1 = 0
+        self.replica_sum_of_users_of1 = 0
+        self.replica_sum_v_ofh = 0
+        self.replica_sum_of_users_ofh = 0
         self.f1 = 0
         self.f2 = 0
         self.f = [0]
         self.variance_f = [0]
         self.t = 0
         self.last_root = 0
+        self.replica_last_root = 0
+        self.replica_activated = False
 
     def new_value(self, v, h):
         """Get values of clients and after callibrating them, it will store their value.
@@ -36,6 +42,52 @@ class Server:
         else:
             self.sum_of_users_ofh += 1
             self.sum_v_ofh += callibrated_v
+        
+    def replica_new_value(self, v, h):
+        """Get replicated values of clients and after callibrating them, it will store their value.
+
+        Args:
+            v (int): The value of replicated client's report which is either 1 of -1
+            h (int): The height of replicated estimation.
+        """
+        callibrated_v = v * self.coef
+        self.replica_last_root = max(self.replica_last_root, h)
+        if h == 0:
+            self.replica_sum_of_users_of1 += 1
+            self.replica_sum_v_of1 += callibrated_v
+        else:
+            self.replica_sum_of_users_ofh += 1
+            self.replica_sum_v_ofh += callibrated_v
+
+    def activate_replica(self):
+        """Activate replica values so they are considered as value when 
+            estimating results
+        """
+        self.sum_of_users_of1 += self.replica_sum_of_users_of1
+        self.sum_v_of1 += self.replica_sum_v_of1
+        self.sum_of_users_ofh += self.replica_sum_of_users_ofh
+        self.sum_v_ofh += self.replica_sum_v_ofh
+        temp = self.last_root
+        self.last_root = self.replica_last_root
+        self.replica_last_root = temp
+        self.replica_activated = True
+
+    def deactivate_replica(self):
+        """Deactivate replica values so they are ignored when estimating results.
+        """
+        self.sum_of_users_of1 -= self.replica_sum_of_users_of1
+        self.sum_v_of1 -= self.replica_sum_v_of1
+        self.sum_of_users_ofh -= self.replica_sum_of_users_ofh
+        self.sum_v_ofh -= self.replica_sum_v_ofh
+        self.replica_sum_of_users_of1 = 0
+        self.replica_sum_of_users_ofh = 0
+        self.replica_sum_v_of1 = 0
+        self.replica_sum_v_ofh = 0
+        temp = self.last_root
+        self.last_root = self.replica_last_root
+        self.replica_last_root = temp
+        self.replica_activated = False
+
 
     def variance_f1(self):
         """Computes varience of f1 which varience of users who are reporting leaf node.
@@ -116,6 +168,37 @@ class Server:
         else:
             w = 0.5 #Just to neutralize its effect.
         freq = w * self.f1 + (1 - w) * self.f2
+        self.t -= 1
+        return np.clip(freq, 0, 1)
+        # self.f.append(freq)
+        # varF = self.compute_variance()
+        # self.variance_f.append(varF)
+        #Reset state of server.
+        # self.sum_v_of1 = 0
+        # self.sum_of_users_of1 = 0
+        # self.sum_v_ofh = 0
+        # self.sum_of_users_ofh = 0
+
+    def go_to_next_round(self):
+        """Predicate frequency of this bit.
+        """
+        if self.replica_activated:
+            raise ValueError('Error! Replica should be deactive to go to next round!')
+        self.t += 1
+        if self.t % 2 == 0:
+            self.f1 = self.f[len(self.f) - 1] + \
+                        (self.sum_v_of1 / self.sum_of_users_of1)
+            t_prime = self.t - 2**self.last_root
+            self.f2 = self.f[t_prime] + \
+                        (self.sum_v_ofh / self.sum_of_users_ofh)
+        else:
+            self.f1 = self.f2 = self.f[len(self.f) - 1] + \
+                        (self.sum_v_of1 / self.sum_of_users_of1)
+        if self.t % 2 == 0:
+            w = self.compute_w()
+        else:
+            w = 0.5 #Just to neutralize its effect.
+        freq = w * self.f1 + (1 - w) * self.f2
         self.f.append(freq)
         varF = self.compute_variance()
         self.variance_f.append(varF)
@@ -124,11 +207,12 @@ class Server:
         self.sum_of_users_of1 = 0
         self.sum_v_ofh = 0
         self.sum_of_users_ofh = 0
+
     def finish(self):
         """Since data should be between -1 and 1, it truncates the data and return the value.
 
         Returns:
             float[]: final frequency of this bit.
         """
-        self.f = np.clip(self.f, 0, 1)
-        return self.f 
+        result = np.clip(self.f, 0, 1)
+        return result 
