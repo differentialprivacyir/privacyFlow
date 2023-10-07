3e184bf7a9f76b0777da6a896276a497949a125e
Milad Golgolnia <golgolniamilad@gmail.com>
2023-10-07 09:45:36 +0330

    1. Changed code to be able read dataset from file and run among many    rounds.
    2. Another method for evaluating algorithm is developed which treats the
       data as integers (Instead of considering them as bit) and compute the Mean Difference.

--- privacy_flow_test.py
+++ privacy_flow_test.py
@@ -1,33 +1,39 @@
 """Contains test of Privacy Flow algorithm and result evaluation for it.
 """
+import math
 from sklearn.metrics import mean_squared_error
 import numpy as np
+import pandas as pd
 from server.manager import PrivacyFlow
 from WrappedClient import WrappeedClient
-import math
 
 # Number of users:
 N = 128000
 # Privacy Budget Levels:
-levels = [(i+1)/10 for i in range(0, 10)]
+levels = [0.5, 1, 2]
 # Determines how many different value types are available in dataset: 
 # (Each bit is responsible for a separate value)
 DATA_SET_SIZE = 8
-# Creates data for each client:
-# dataSet = [[i for i in np.random.randint(2 ** DATA_SET_SIZE - 1, size=N)]]
-dataSet = [[math.floor(i) for i in np.random.normal(100, 10, size=N)]]
-# Determine selected privacy level of each client:
-clientSelectedLevel = np.random.randint(len(levels), size=N)
-# Creates actual clients:
-clients = [WrappeedClient(DATA_SET_SIZE, levels, clientSelectedLevel[i], 10) for i in range(N)]
-# Initialize Server:
-server = PrivacyFlow(None, levels, DATA_SET_SIZE)
 # Number of rounds to run the code:
 ROUND_CHANGES = 1
+# Creates data for each client:
+# dataSet = [[i for i in np.random.randint(2 ** DATA_SET_SIZE - 1, size=N)]]
+# dataSet = [[math.floor(i) for i in np.random.normal(100, 10, size=N)]]
+# Read dataset from file:
+csvContent = pd.read_csv('./uniform.csv')
+dataSet = np.transpose(csvContent.to_numpy())
+# Determine selected privacy level of each client:
+# clientSelectedLevel = np.random.randint(len(levels), size=N)
+clientSelectedLevel = [0] * 42667 + [1] * 42667 + [2] * 42666
+# Creates actual clients:
+clients = [WrappeedClient(DATA_SET_SIZE, levels, clientSelectedLevel[i], ROUND_CHANGES) for i in range(N)]
+# Initialize Server:
+server = PrivacyFlow(None, levels, DATA_SET_SIZE)
 # Prepare to keep results of estimations:
 estimations = []
 # Start the test
 for i in range(ROUND_CHANGES):
+    print(f'round {i} started')
     # Prepare server data:
     serverData = {i: [] for i in levels}
     for j in range(N):
@@ -46,22 +52,45 @@ for i in range(ROUND_CHANGES):
     # Call this method if you want to get estimation of data:
     estimations.append([])
     for l in levels:
-        estimations[0].append(server.estimate(l))
+        estimations[i].append(server.estimate(l))
     # Always call this method when you are going to the next round:
     server.next_round()
 
+# print(server.finish())
 # Compute bit frequency of real values:
-bitRepresentationOfDataSet = [bin(i)[2:].zfill(DATA_SET_SIZE) for i in dataSet[0]]
-numericalBitRepresentationDataSet = [[int(char) for char in list(data)] \
+frequencies = []
+for singleRound in dataSet:
+    bitRepresentationOfDataSet = [bin(i)[2:].zfill(DATA_SET_SIZE) for i in singleRound]
+    numericalBitRepresentationDataSet = [[int(char) for char in list(data)] \
                                         for data in bitRepresentationOfDataSet]
-frequencies = np.sum(numericalBitRepresentationDataSet, axis=0)
-normalized = frequencies / N
+    frequencies.append(np.sum(numericalBitRepresentationDataSet, axis=0))
+normalized = np.array(frequencies) / N
 
 # Evaluate mean error:
-error = []
-for index, estimation in enumerate(estimations[0]):
-    print(f'Evaluation for level eps = {levels[index]}') 
-    for i, _ in enumerate(normalized):  # calculating errors
-        error.append(abs(estimation[i] - normalized[i]) * 100)
-        print("index:", i, "-> Estimated:", estimation[i], " Real:", normalized[i], " Error: %", int(error[-1]))
-    print("Global Mean Square Error:", mean_squared_error(normalized, estimation))
+for r in range(ROUND_CHANGES):
+    print(f'\n\n\n ========================================== \nResults of Round {r}:\n==========================================')
+    error = []
+    for index, estimation in enumerate(estimations[r]):
+        print(f'Evaluation for level eps = {levels[index]}')
+        for i, _ in enumerate(normalized[r]):  # calculating errors
+            error.append(abs(estimation[i] - normalized[r][i]) * 100)
+            print("index:", i, "-> Estimated:", estimation[i], " Real:", normalized[r][i], " Error: %", int(error[-1]))
+        print("Global Mean Square Error:", mean_squared_error(normalized[r], estimation))
+
+
+meanOfRounds = np.mean(dataSet, axis=1)
+print('Real Mean of rounds is:', meanOfRounds)
+outputMean = []
+for r in range(ROUND_CHANGES):
+    outputMean.append([])
+    for index, estimationAtL in enumerate(estimations[r]):
+        ROUND_MEAN = 0
+        REAL_ROUND_MEAN = 0
+        for index2, number in enumerate(estimationAtL):
+            ROUND_MEAN += (number*(N) * 2 ** (len(estimationAtL) - 1 - index2))
+            REAL_ROUND_MEAN += (normalized[r][index2]*(N) * 2 ** (len(estimationAtL) - 1 - index2))
+        ROUND_MEAN /= N
+        REAL_ROUND_MEAN /= N
+        print(f'Mean Difference at round {r} and level {levels[index]}:', abs(ROUND_MEAN - meanOfRounds[r]), abs(ROUND_MEAN - REAL_ROUND_MEAN))
+        outputMean[r].append(ROUND_MEAN)
+print("Estimated Mean is:", outputMean)
